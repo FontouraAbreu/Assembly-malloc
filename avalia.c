68238adaaa416b6abc4fa632f64aa9b62c11632d
guiusa <guiusepeoneda@gmail.com>
2023-12-04 11:45:31 -0300

    fixing a bug where mergeNodes accessed invalid memory

--- avalia.c
+++ avalia.c
@@ -1,48 +1,12 @@
 #include <stdio.h>
 #include "meuAlocador.h"
 
-int main (long int argc, char** argv) {
-  void *a,*b,*c,*d,*e;
+int main (int argc, char** argv) {
+ 	void *a;
+	iniciaAlocador(); 
 
-  iniciaAlocador(); 
-  imprimeMapa();
-  // 0) estado inicial
+	a = (char *) alocaMem(10 * sizeof(char));
+	liberaMem(a);
 
-  a=(void *) alocaMem(100);
-  imprimeMapa();
-  b=(void *) alocaMem(130);
-  imprimeMapa();
-  c=(void *) alocaMem(120);
-  imprimeMapa();
-  d=(void *) alocaMem(110);
-  imprimeMapa();
-  // 1) Espero ver quatro segmentos ocupados
-
-  liberaMem(b);
-  imprimeMapa(); 
-  liberaMem(d);
-  imprimeMapa(); 
-  // 2) Espero ver quatro segmentos alternando
-  //    ocupados e livres
-
-  b=(void *) alocaMem(50);
-  imprimeMapa();
-  d=(void *) alocaMem(90);
-  imprimeMapa();
-  e=(void *) alocaMem(40);
-  imprimeMapa();
-  // 3) Deduzam
-	
-  liberaMem(c);
-  imprimeMapa(); 
-  liberaMem(a);
-  imprimeMapa();
-  liberaMem(b);
-  imprimeMapa();
-  liberaMem(d);
-  imprimeMapa();
-  liberaMem(e);
-  imprimeMapa();
-   // 4) volta ao estado inicial
-  finalizaAlocador();
+	finalizaAlocador();
 }
